79b91bb2da94497fe2e6fcd4afda3a09457e76da
Vian-netizen <groudon737@gmail.com>
2026-07-02 15:05:49 -0500

    Batch 3 // AppLayout, Sidebar, AuthContext

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -18,7 +18,13 @@ export default function AppLayout() {
   return (
     <div className="min-h-screen bg-background">
       <div className="scanline-overlay" />
-      <Sidebar user={user} clearanceLevel={clearanceLevel} isAdmin={isAdmin} />
+
+      <Sidebar
+        user={user}
+        clearanceLevel={clearanceLevel}
+        isAdmin={isAdmin}
+      />
+
       <main className="lg:ml-[240px] min-h-screen">
         <div className="p-4 md:p-6 lg:p-8 pt-14 lg:pt-8">
           <Outlet context={{ user, personnel, clearanceLevel, isAdmin }} />
