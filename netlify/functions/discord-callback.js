7c35d63b550cb8bb3c49f7138036758fafce2c02
Vian-netizen <groudon737@gmail.com>
2026-07-01 21:56:47 -0500

    callback again

--- netlify/functions/discord-callback.js
+++ netlify/functions/discord-callback.js
@@ -1,6 +1,49 @@
-export const handler = async () => {
+export const handler = async (event) => {
+  const code = event.queryStringParameters?.code;
+
+  if (!code) {
+    return {
+      statusCode: 400,
+      body: "Missing code"
+    };
+  }
+
+  const params = new URLSearchParams();
+  params.append("client_id", process.env.DISCORD_CLIENT_ID);
+  params.append("client_secret", process.env.DISCORD_CLIENT_SECRET);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", process.env.DISCORD_REDIRECT_URI);
+
+  const tokenRes = await fetch("https://discord.com/api/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: params.toString()
+  });
+
+  const token = await tokenRes.json();
+
+  if (!tokenRes.ok) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(token)
+    };
+  }
+
+  const userRes = await fetch("https://discord.com/api/users/@me", {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`
+    }
+  });
+
+  const user = await userRes.json();
+
   return {
-    statusCode: 200,
-    body: "callback works"
+    statusCode: 302,
+    headers: {
+      Location: `/?discord_id=${user.id}&username=${user.username}`
+    }
   };
 };
