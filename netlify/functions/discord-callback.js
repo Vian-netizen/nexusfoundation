16c527db3beeab92a02a2ce86a2f3ae3299fbe55
Vian-netizen <groudon737@gmail.com>
2026-07-01 21:36:09 -0500

    Update discord callback OAuth flow

--- netlify/functions/discord-callback.js
+++ netlify/functions/discord-callback.js
@@ -8,11 +8,42 @@ export async function handler(event) {
     };
   }
 
+  const params = new URLSearchParams();
+  params.append("client_id", process.env.DISCORD_CLIENT_ID);
+  params.append("client_secret", process.env.DISCORD_CLIENT_SECRET);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", process.env.DISCORD_REDIRECT_URI);
+
+  const tokenRes = await fetch("https://discord.com/api/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: params.toString()
+  });
+
+  const tokenData = await tokenRes.json();
+
+  if (!tokenRes.ok) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(tokenData)
+    };
+  }
+
+  const userRes = await fetch("https://discord.com/api/users/@me", {
+    headers: {
+      Authorization: `Bearer ${tokenData.access_token}`
+    }
+  });
+
+  const user = await userRes.json();
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Callback received",
-      code
-    })
+    statusCode: 302,
+    headers: {
+      Location: `/login?discord_id=${user.id}&username=${user.username}`
+    }
   };
 }
